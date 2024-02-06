d00db582da7b9437b75322aac5624635b2abd85e
SoPhors <91106971+PhoSophors@users.noreply.github.com>
2024-02-06 14:47:09 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+;
+function App() {
+  return (
+    <div className="App" >
+      <div>
+        <h1 className="text-6xl text-red-500">Hello, Tailwind CSS!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get("/api", (req, res) => {
+  res.json({ "users": ["userOne", "userTwo", "userThree"] });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
