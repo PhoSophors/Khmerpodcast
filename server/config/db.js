af2cafa13cb65c6fe7e0e6b8379d1b0847ccda49
PhoSophors <phosophors.097@gmail.com>
2024-05-11 00:51:16 +0700

    update api

--- server/config/db.js
+++ server/config/db.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
-        process.exit(1); // Exit process with failure
+        process.exit(1); 
     }
 };
 
