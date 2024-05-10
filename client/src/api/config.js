91b1626fff1c704520b66ed2f3ecc0f4d622b344
PhoSophors <phosophors.097@gmail.com>
2024-05-11 01:03:15 +0700

    d

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,8 +1,8 @@
 
 
-const api_url = process.env.REACT_APP_API;
+// const api_url = process.env.REACT_APP_API_BACKEND;
 
-// const api_url = "https://khmerpodcast.onrender.com";
+const api_url = "https://khmerpodcast.onrender.com";
 
 if (!api_url) {
   // throw new Error("REACT_APP_BACKEND_API is not defined");
