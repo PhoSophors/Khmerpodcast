78ce0b5a118129785bea89b15d52e344df5a67b6
PhoSophors <phosophors.097@gmail.com>
2024-05-17 11:19:22 +0700

    update maxAge session in server

--- server/server.js
+++ server/server.js
@@ -57,6 +57,7 @@ app.use(
       secure: process.env.NODE_ENV === "production", // Set secure to true in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Set sameSite to none in production
       httpOnly: true, // Ensures the cookie is sent only over HTTP(S), not client JavaScript
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     },
   })
 );
