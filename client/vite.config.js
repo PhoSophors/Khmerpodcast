b927a32dc81c0e7e2c3f9512e87d03eb5a2311f7
PhoSophors <phosophors.097@gmail.com>
2024-05-27 22:14:05 +0700

    update 404: NOT_FOUND

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  
+  plugins: [react()],
+  server: {
+    host: true,
+    fs: {
+      strict: true,
+    },
+    // Set the `base` to be the root
+    base: '/',
+    // Enable the history API fallback
+    historyApiFallback: true,
+  },
 })
